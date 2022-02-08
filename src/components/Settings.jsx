a7a9e28bdcf793ea4618edfc8a02531d14efcec5
TheComeBackGuy <dennis.hart@gmail.com>
2022-02-08 15:53:36 -0500

    added difficulty back into settings.

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,3 +1,6 @@
+import { activeGame, numberOfColumns, numberOfRows } from "../data/atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
 import { GiBrain } from "react-icons/gi";
 import React from "react";
 import styled from "styled-components";
@@ -14,6 +17,7 @@ const DifficultyItem = styled.li`
   font-size: 35px;
   color: ${(props) => props.theme.color};
   margin: 5px 10px 0 0;
+  cursor: pointer;
   &:last-child {
     margin-right: 0;
   }
@@ -46,19 +50,44 @@ const Footer = styled.div`
   font-weight: 200;
 `;
 export default function Settings() {
+  const setRows = useSetRecoilState(numberOfRows);
+  const setCols = useSetRecoilState(numberOfColumns);
+  const gameIsActive = useRecoilValue(activeGame);
+
   return (
     <div>
       <h2>Settings</h2>
       <p>Choose your difficulty</p>
       <DifficultyList>
         <DifficultyItem theme={easy}>
-          <GiBrain />
+          <GiBrain
+            onClick={(e) => {
+              if (!gameIsActive) {
+                setCols(4);
+                setRows(12);
+              }
+            }}
+          />
         </DifficultyItem>
         <DifficultyItem theme={medium}>
-          <GiBrain />
+          <GiBrain
+            onClick={(e) => {
+              if (!gameIsActive) {
+                setCols(6);
+                setRows(12);
+              }
+            }}
+          />
         </DifficultyItem>
         <DifficultyItem theme={hard}>
-          <GiBrain />
+          <GiBrain
+            onClick={(e) => {
+              if (!gameIsActive) {
+                setCols(10);
+                setRows(14);
+              }
+            }}
+          />
         </DifficultyItem>
       </DifficultyList>
       <Footer>
